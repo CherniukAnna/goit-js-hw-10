74b254760ac3176ce66e0161f41de6a27ed3e969
CherniukAnna <chernukanna@gmail.com>
2023-02-02 19:24:49 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-// import './css/styles.css';
 
-// const DEBOUNCE_DELAY = 300;
-
-//
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
